f5bb570ea585ef84823e42e78b5936c3ccaee70b
Clara <clara.gabrielle@youxgroup.com.br>
2023-03-13 18:38:05 -0300

    REFACTOR: refatorando logica para pegar dia e hora comecado e terminado da atividade

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,37 +9,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Digite dia que você começou a task X: ");
-        Integer starterDay = sc.nextInt();
-        System.out.println("Digite mês que você começou a task X: ");
-        Integer starterMonth = sc.nextInt();
-        System.out.println("Digite ano que você começou a task X: ");
-        Integer starterYear = sc.nextInt();
-        System.out.println("Digite a hora que você começou a task X: ");
-        Integer starterHour = sc.nextInt();
-        System.out.println("Digite os minutos que você começou a task X: ");
-        Integer starterMinute = sc.nextInt();
+        System.out.println("Digite o dia que você começou a task X: (dd/MM/yyyy)");
+        String startDate = sc.next();
+        System.out.println("Digite o horário que você começou a task X: (dd/MM/yyyy)");
+        String startTime = sc.next();
 
-        String starterMoment = String.format("%02d/%02d/%d %02d:%02d", starterDay, starterMonth, starterYear, starterHour, starterMinute);
-
+        String starterDate = startDate + " " + startTime;
         DateTimeFormatter starterFormater = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-        LocalDateTime starter = LocalDateTime.parse(starterMoment, starterFormater);
+        LocalDateTime starter = LocalDateTime.parse(starterDate, starterFormater);
 
-        System.out.println("Digite dia que você terminou a task X: ");
-        Integer endDay = sc.nextInt();
-        System.out.println("Digite mês que você terminou a task X: ");
-        Integer endMonth = sc.nextInt();
-        System.out.println("Digite ano que você terminou a task X: ");
-        Integer endYear = sc.nextInt();
-        System.out.println("Digite a hora que você terminou a task X: ");
-        Integer endHour = sc.nextInt();
-        System.out.println("Digite os minutos que você terminou a task X: ");
-        Integer endMinute = sc.nextInt();
+        System.out.println("Digite o dia que você terminou a task X: (dd/MM/yyyy HH:mm)");
+        String endDate = sc.next();
+        System.out.println("Digite o horário que você terminou a task X: (dd/MM/yyyy HH:mm)");
+        String endTime = sc.next();
 
-        String endMoment = String.format("%02d/%02d/%d %02d:%02d", endDay, endMonth, endYear, endHour, endMinute);
+        String finalTime = endDate + " " + endTime;
 
         DateTimeFormatter endFormater = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-        LocalDateTime end = LocalDateTime.parse(endMoment, endFormater);
+        LocalDateTime end = LocalDateTime.parse(finalTime, endFormater);
 
         Duration duration = Duration.between(starter, end);
 
